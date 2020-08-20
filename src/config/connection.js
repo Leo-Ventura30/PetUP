e9c16c65e533ce8a9122d776433756ffa73b13ae
leo-ventura30 <leonardo.ventura@outlook.com>
2020-08-19 21:29:11 -0300

    chore: adjust db

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,10 +1,6 @@
 // const path = require("path");
 module.exports = {
-  dialect: "postgres",
-  host: "192.168.15.32",
-  username: "postgres",
-  password: "2585Leo",
-  database: "postgres",
+  uri: process.env.DATABASE_URL,
   operatorAliases: false,
   define: {
     timestamps: true,
